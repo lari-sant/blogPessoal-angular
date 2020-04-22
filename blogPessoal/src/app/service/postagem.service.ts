ec0baee2622f0cecf1e778aa23824645b36f2328
unknown <teixeiralary99@gmail.com>
2020-04-22 01:42:27 -0300

    Criação do enPoint getAllPostagens

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
 
-  constructor() { }
-  
+  constructor(private http: HttpClient) { }
+
+getAllPostagens(){
+  return this.http.get('htto://31.220.57.14:8080/postagens')
+}
+
 }
